bc059e614a2ecc96980a502f462f183825f3a61a
kingsley julius <coletkingconcept@gmail.com>
2026-04-20 13:00:54 +0100

    fix issue

--- src/utils/MenuContent.tsx
+++ src/utils/MenuContent.tsx
@@ -12,6 +12,7 @@ import {
   MdHandshake,
 } from "react-icons/md";
 import { FiLogIn, FiUserPlus } from "react-icons/fi";
+import { GiMassDriver } from "react-icons/gi";
 
 export const menuItems = [
   { name: "Dashboard", link: "/dashboard", icon: MdDashboard },
@@ -51,6 +52,7 @@ export const menuItems = [
 ];
 
 export const guestMenuItems = [
+ 
   { name: "Sign In", link: "/auth/login", icon: FiLogIn },
   { name: "Sign Up", link: "/auth/register", icon: FiUserPlus },
   {
@@ -58,4 +60,5 @@ export const guestMenuItems = [
     link: "/partner-with-us",
     icon: MdHandshake,
   },
+   { name: "Become a Driver", link: "https://host.muvment.ng/earn", icon: GiMassDriver },
 ];
